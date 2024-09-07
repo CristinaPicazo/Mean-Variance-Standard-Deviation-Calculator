f8d11f39af39ed3145b55b01f2a7bceedfbd9e40
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-09-07 20:16:46 +0200

    changed dictionary

--- mean_var_std.py
+++ mean_var_std.py
@@ -5,7 +5,7 @@ def calculate(list):
         np_list = np.array(list).reshape(3,3)
 
         mean = {'mean':[np_list.mean(axis=0),np_list.mean(axis=1),np_list.mean()]}
-        variance = {'mean':[np_list.var(axis=0),np_list.var(axis=1),np_list.var()]}
+        variance = {'variance':[np_list.var(axis=0),np_list.var(axis=1),np_list.var()]}
         standard_deviation = {'standard deviation':[np_list.std(axis=0),np_list.std(axis=1),np_list.std()]}
         max = {'max':[np_list.max(axis=0),np_list.max(axis=1),np_list.max()]}
         min = {'min':[np_list.min(axis=0),np_list.min(axis=1),np_list.min()]}
@@ -13,7 +13,13 @@ def calculate(list):
 
         
         # Dictionary with all the results
-        calculations = {mean,variance,standard_deviation,max,min,sum}
+        calculations = {}
+        calculations['mean'] = [np.mean(np_list,axis=0),np.mean(np_list,axis=1),np.mean(np_list.flatten())]
+        calculations['variance']=[np.var(np_list,axis=0),np.var(np_list,axis=1),np.var(np_list.flatten())]
+        calculations['standard deviation']=[np.std(np_list,axis=0),np.std(np_list,axis=1),np.std(np_list.flatten())]
+        calculations['max']=[np.max(np_list,axis=0),np.max(np_list,axis=1),np.max(np_list.flatten())]
+        calculations['min']=[np.min(np_list,axis=0),np.min(np_list,axis=1),np.min(np_list.flatten())]
+        calculations['sum']=[np.sum(np_list,axis=0),np.sum(np_list,axis=1),np.sum(np_list.flatten())]
 
         return calculations
     except:
